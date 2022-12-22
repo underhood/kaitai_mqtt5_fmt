b11ca23e5948db2118de4518518b1f0ec8f03eee
Timotej S <timotejs@gmail.com>
2022-12-22 10:36:20 +0700

    initial parser and tests

---
+++ mqtt5.ksy
@@ -0,0 +1,107 @@
+meta:
+  id: mqtt5
+  file-extension: bin
+  bit-endian: be
+
+seq:
+  - id: mqtt_message
+    type: mqtt_msg
+
+enums:
+# [MQTT-2.1.2]
+  mqtt_cpt_enum:
+    1: connect
+    2: connack
+    3: publish
+    4: puback
+    5: pubrec
+    6: pubrel
+    7: pubcomp
+    8: subscribe
+    9: suback
+    10: unsubscribe
+    11: unsuback
+    12: pingreq
+    13: pingresp
+    14: disconnect
+    15: auth
+
+types:
+  mqtt_cpt:
+    seq:
+      - id: cpt
+        type: b4
+        enum: mqtt_cpt_enum
+
+  mqtt_msg:
+    seq:
+      - id: message
+        type:
+          switch-on: 'mqtt_cpt.cpt'
+          cases:
+            mqtt_cpt_enum::pingreq: mqtt_pingreq
+            mqtt_cpt_enum::pingresp: mqtt_pingresp
+    instances:
+      # I do this because first 4 bits are signifying packet type and second 4 bits are packet type dependent
+      # when I try to do sth. like:
+      # seq:
+      #   - id: cpt
+      #     type: b4
+      #   - id: flags
+      #     type:
+      #       switch-on: 'cpt'
+      #       cases:
+      #         ...
+      # the parser chosen in the switch-on will start parsing from the second byte
+      mqtt_cpt:
+        type: mqtt_cpt
+        pos: 0
+        size: 1
+
+  mqtt_pingreq:
+    seq:
+      - id: cpt
+        type: b4
+        valid:
+          expr: '_ == 0xC' # for some reason cannot use contents with b4 type
+      - id: flags
+        type: b4
+        valid:
+          expr: '_ == 0'
+      - id: rem_length
+        type: mqtt_varint
+        valid:
+          expr: '_.val == 0'
+  mqtt_pingresp:
+    seq:
+     - id: cpt
+       type: b4
+       valid:
+         expr: '_ == 0xD' # for some reason cannot use contents with b4 type
+     - id: flags
+       type: b4
+       valid:
+         expr: '_ == 0'
+     - id: rem_length
+       type: mqtt_varint
+       valid:
+         expr: '_.val == 0'
+
+  mqtt_varint:
+    seq:
+      - id: bytes
+        type: u1
+        repeat: until
+        repeat-until: '(_ & (1 << 7)) == 0'
+        valid:
+# MQTT spec allows max 4 bytes even though unlimited is teoretically possible
+          expr: '(bytes.size <= 4)
+          and (val <= 127 ? bytes.size == 1 :
+            (val <= 16383 ? bytes.size == 2 :
+              (val <= 2097151 ? bytes.size == 3 : bytes.size == 4)))'
+    instances:
+      val:
+        value: '(bytes[0] & 0x7F)
+        + (bytes.size > 1 ? (bytes[1] & 0x7F) * (1 << 7) : 0)
+        + (bytes.size > 2 ? (bytes[2] & 0x7F) * (1 << 14) : 0)
+        + (bytes.size > 3 ? (bytes[3] & 0x7F) * (1 << 21) : 0)'
---
+++ tests/test.rb
@@ -0,0 +1,78 @@
+require 'kaitai/struct/struct'
+
+require_relative '../mqtt5.rb'
+
+require "test/unit"
+
+class TestMqttVarintParser < Test::Unit::TestCase
+  def test_varint_max
+    assert_equal(268435455, Mqtt5::MqttVarint.from_file('data_samples/vbi_max_valid').val )
+  end
+  def test_varint_oversize
+    assert_raise(Kaitai::Struct::ValidationExprError) { Mqtt5::MqttVarint.from_file('data_samples/vbi_oversize') }
+  end
+  def test_varint_bloated
+    # tests parser validates [MQTT-1.5.5-1] quoting:
+    # "The encoded value MUST use the minimum number of bytes necessary to represent the value"
+    assert_raise(Kaitai::Struct::ValidationExprError) { Mqtt5::MqttVarint.from_file('data_samples/vbi_bloated') }
+  end
+  def test_varint_null
+    bytes = "\x00"
+    assert_equal(0, Mqtt5::MqttVarint.new(Kaitai::Struct::Stream.new(bytes)).val)
+  end
+  def test_varint_limits
+    bytes127 = "\x7F"
+    varint = Mqtt5::MqttVarint.new(Kaitai::Struct::Stream.new(bytes127))
+    assert_equal(127, varint.val)
+    assert_equal(1, varint.bytes.size)
+
+    bytes128 = "\x80\x01"
+    varint = Mqtt5::MqttVarint.new(Kaitai::Struct::Stream.new(bytes128))
+    assert_equal(128, varint.val)
+    assert_equal(2, varint.bytes.size)
+
+    bytes16383 = "\xFF\x7F"
+    assert_equal(16383, Mqtt5::MqttVarint.new(Kaitai::Struct::Stream.new(bytes16383)).val)
+    bytes16384 = "\x80\x80\x01"
+    assert_equal(16384, Mqtt5::MqttVarint.new(Kaitai::Struct::Stream.new(bytes16384)).val)
+    bytes2097151 = "\xFF\xFF\x7F"
+    assert_equal(2097151, Mqtt5::MqttVarint.new(Kaitai::Struct::Stream.new(bytes2097151)).val)
+    bytes2097152 = "\x80\x80\x80\x01"
+    assert_equal(2097152, Mqtt5::MqttVarint.new(Kaitai::Struct::Stream.new(bytes2097152)).val)
+    bytes268435455 = "\xFF\xFF\xFF\x7F"
+    assert_equal(268435455, Mqtt5::MqttVarint.new(Kaitai::Struct::Stream.new(bytes268435455)).val)
+  end
+end
+
+class TestMqttPingParsers < Test::Unit::TestCase
+  def test_ping_parse
+    ping_with_len = "\xC0\05" # test ping with non 0 payload len fails to parse
+    ping_with_flags = "\xC3\x00" # tests ping with flags set to non0 fails
+    ping_no_len = "\xC0" # tests ping without remaining length fails
+    ping_valid = "\xC0\00"
+
+    assert_raise( EOFError ) { Mqtt5::MqttMsg.new(Kaitai::Struct::Stream.new(ping_no_len)) }
+    assert_raise( Kaitai::Struct::ValidationExprError ) { Mqtt5::MqttMsg.new(Kaitai::Struct::Stream.new(ping_with_len)) }
+    assert_raise( Kaitai::Struct::ValidationExprError ) { Mqtt5::MqttMsg.new(Kaitai::Struct::Stream.new(ping_with_flags)) }
+
+    assert_nothing_raised { ping = Mqtt5::MqttMsg.from_file('data_samples/pingreq_valid') }
+    ping = Mqtt5::MqttMsg.from_file('data_samples/pingreq_valid')
+    assert_equal(ping.mqtt_cpt.cpt, :mqtt_cpt_enum_pingreq)
+  end
+  def test_pingresp_parse
+    pingresp_with_len = "\xD0\05" # test ping with non 0 payload len fails to parse
+    pingresp_with_flags = "\xD3\x00" # tests ping with flags set to non0 fails
+    pingresp_no_len = "\xD0" # tests ping without remaining length fails
+    pingresp_valid = "\xD0\00"
+    ping_valid = "\xC0\00"
+
+    assert_raise( EOFError ) { Mqtt5::MqttMsg.new(Kaitai::Struct::Stream.new(pingresp_no_len)) }
+    assert_raise( Kaitai::Struct::ValidationExprError ) { Mqtt5::MqttMsg.new(Kaitai::Struct::Stream.new(pingresp_with_len)) }
+    assert_raise( Kaitai::Struct::ValidationExprError ) { Mqtt5::MqttMsg.new(Kaitai::Struct::Stream.new(pingresp_with_flags)) }
+
+    assert_nothing_raised { pingresp = Mqtt5::MqttMsg.new(Kaitai::Struct::Stream.new(pingresp_valid)) }
+    pingresp = Mqtt5::MqttMsg.new(Kaitai::Struct::Stream.new(pingresp_valid))
+    assert_equal(pingresp.mqtt_cpt.cpt, :mqtt_cpt_enum_pingresp)
+    assert_raise( Kaitai::Struct::ValidationExprError ) { Mqtt5::MqttPingresp.new(Kaitai::Struct::Stream.new(ping_valid)) }
+  end
+end
